200ef861d1be068602cd8834c65c32d66cb96c54
Eduardo Tanabe <e_tanabe@yahoo.com.br>
2021-01-28 22:45:16 -0400

    Mudança de algumas configurações, como quantidade de objeto que aparecerá em cada página e mudança to enderço por cada entidade

--- spring-data-jpa-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ spring-data-jpa-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.luv2code.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
+//@RepositoryRestResource(path = "members")   // com esta anotação estarei mudando o caminho, assim, o Spring estará mudando de http://localhost:8989/employees para http://localhost:8989/members
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!!
